da0c9fd99f3d9e76a9c765d8f462786031b27ca4
Mulang Su <sumulang@gmail.com>
2020-04-04 09:56:43 +0100

    fetched like count of videos as well

--- popupScript.js
+++ popupScript.js
@@ -55,13 +55,14 @@ function fetchSearchResults(depth, completion, pageToken) {
         "https://www.googleapis.com/youtube/v3/search", params,
         (response) => {
             let videoIds = getCommaSeparatedVideoIds(response);
-            getVideoDurations(videoIds, (durations) => {
+            getVideoDetails(videoIds, (details) => {
                 const zip = (arr1, arr2) => arr1.map((k, i) => [k, arr2[i]]);
-                let newSearchResults = zip(response.items, durations).map(x => ({
+                let newSearchResults = zip(response.items, details).map(x => ({
                     videoId: x[0].id.videoId,
                     title: x[0].snippet.title,
                     description: x[0].snippet.description,
-                    duration: x[1],
+                    duration: x[1].duration,
+                    likeCount: x[1].likeCount,
                     thumbnail: x[0].snippet.thumbnails.medium.url
                 }));
                 searchResults = searchResults.concat(newSearchResults);
@@ -79,12 +80,15 @@ function getCommaSeparatedVideoIds(searchResponse) {
     return searchResponse.items.map(x => x.id.videoId).join(",");
 }
 
-function getVideoDurations(videoIds, completion) {
+function getVideoDetails(videoIds, completion) {
     $.get(
         "https://www.googleapis.com/youtube/v3/videos",
-        {part: "contentDetails", id: videoIds, key: apiKey},
+        {part: "contentDetails,statistics", id: videoIds, key: apiKey},
         (response) => {
-            completion(response.items.map(x => x.contentDetails.duration));
+            completion(response.items.map(x => ({
+                duration: x.contentDetails.duration,
+                likeCount: x.statistics.likeCount
+            })));
         }
     );
 }
