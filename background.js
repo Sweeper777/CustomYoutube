4a15b8391838b6ffea85ddae489fbd8b0dd5b229
Mulang Su <sumulang@gmail.com>
2020-05-10 15:27:43 +0100

    showed the popup

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 function input() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         chrome.tabs.sendMessage(tabs[0].id, {action: "getVideos"}, function(response) {
-            
+            if (response === "success") {
+                chrome.tabs.create({url: "popup.html#window"});
+            }
         });  
     });
 }
