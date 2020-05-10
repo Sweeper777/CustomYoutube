a376b8d8d07e766c3c0f258c8b6f0e24a744dafb
Mulang Su <sumulang@gmail.com>
2020-05-10 15:27:32 +0100

    got the video details

--- contentScript.js
+++ contentScript.js
@@ -4,6 +4,24 @@ function parseHref(href) {
     return match[1] ?? undefined;
 }
 
+function getVideoDetails(videoIds, completion) {
+    $.get(
+        "https://www.googleapis.com/youtube/v3/videos",
+        {part: "contentDetails,statistics,snippet", id: videoIds, key: apiKey},
+        (response) => {
+            completion(response.items.map(x => ({
+                videoId: x.videoId,
+                title: x.snippet.title,
+                description: x.snippet.description,
+                duration: x.duration,
+                likeCount: x.likeCount,
+                viewCount: x.viewCount,
+                thumbnail: x.snippet.thumbnails.medium.url
+            })));
+        }
+    );
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log("message received");
@@ -11,7 +29,10 @@ chrome.runtime.onMessage.addListener(
             var allVideoElements = $("ytd-rich-grid-video-renderer, ytd-grid-video-renderer, ytd-video-renderer");
             var aTags = allVideoElements.children("a#thumbnail").get();
             var hrefs = aTags.map(x => $(x).attr("href"));
-            var videoIds = hrefs.map(parseHref).filter(x => x);
-            
+            var videoIds = hrefs.map(parseHref).filter(x => x).slice(0, 50);
+            getVideoDetails(videoIds.join(","), response => {
+                chrome.storage.local.set({cachedResults: response}, () => sendResponse("success"));
+            })
+            return true
         }
     });
