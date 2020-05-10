8ba2eb4069a837db61fc40f1081204d0782639b3
Mulang Su <sumulang@gmail.com>
2020-05-10 14:36:16 +0100

    added parseHref

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,9 @@
+function parseHref(href) {
+    var regex = /^\/watch\?v=(.+)$/;
+    var match = regex.exec(href);
+    return match[1] ?? undefined;
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         }
