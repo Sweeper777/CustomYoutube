90695dede45e1d8412c657304f0b987f869a374a
Mulang Su <sumulang@gmail.com>
2020-06-10 11:04:13 +0100

    fixed the bug of not finding any videos

--- contentScript.js
+++ contentScript.js
@@ -9,7 +9,7 @@ chrome.runtime.onMessage.addListener(
         console.log("message received");
         if (request.action === "getVideos") {
             var allVideoElements = $("ytd-rich-grid-video-renderer, ytd-grid-video-renderer, ytd-video-renderer");
-            var aTags = allVideoElements.children("a#thumbnail").get();
+            var aTags = allVideoElements.find("a#thumbnail").get();
             var hrefs = aTags.map(x => $(x).attr("href"));
             var videoIds = hrefs.map(parseHref).filter(x => x).slice(0, 50);
             sendResponse(videoIds)
