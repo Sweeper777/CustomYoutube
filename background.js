680e70688355c55ad853003aa1104910bd1a8b11
Mulang Su <sumulang@gmail.com>
2020-06-13 12:15:52 +0100

    accepted a pair of numbers as omnibox input

--- background.js
+++ background.js
@@ -54,19 +54,26 @@ function getVideoDetailsImpl(videoIdChunks, completion) {
     }
 }
 
-function input() {
-    console.log("input!")
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        console.log("queried!")
-        chrome.tabs.sendMessage(tabs[0].id, {action: "getVideos"}, function(videoIds) {
-            if (videoIds) {
-                getVideoDetails(videoIds.join(","), response => {
-                    chrome.storage.local.set({cachedResults: response}, 
-                        () => chrome.tabs.create({url: "popup.html#window"}));
-                })
-            }
-        });  
-    });
+function input(string) {
+    var regex = /^(\d+),(\d+)$/;
+    var match = regex.exec(string);
+    if (match) {
+        var start = Number(match[1]);
+        var end = Number(match[2]);
+        if (start > end || end - start > 249) {
+            return;
+        }
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "getVideos", startIndex: start - 1, endIndex: end}, function(videoIds) {
+                if (videoIds) {
+                    getVideoDetails(videoIds, response => {
+                        chrome.storage.local.set({cachedResults: response}, 
+                            () => chrome.tabs.create({url: "popup.html#window"}));
+                    })
+                }
+            });  
+        });
+    }
 }
 chrome.omnibox.onInputEntered.addListener(input);
 console.log("background script run!");
